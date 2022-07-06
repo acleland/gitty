1956ce6dc2a6e44098733a5a3e86372b39679cc1
Andrea Cleland <andreaclelan@gmail.com>
2022-07-06 14:40:50 -0600

    test passing for redirect to /posts using mocks

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/services/github');
+
 describe('Gitty routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -13,6 +15,21 @@ describe('Gitty routes', () => {
     expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
     );
+  });
+
+  it('should login and redirect users to /api/v1/posts', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
 
   afterAll(() => {
