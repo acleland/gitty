96224f97aa116b732f56f2c21d08c3bf76913e93
Andrea Cleland <andreaclelan@gmail.com>
2022-07-06 14:47:19 -0600

    added gitty_posts table

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
 DROP TABLE IF EXISTS github_users;
+DROP TABLE IF EXISTS gitty_posts;
 
 CREATE TABLE github_users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -9,3 +10,9 @@ CREATE TABLE github_users (
   email VARCHAR
 );
 
+CREATE TABLE gitty_posts (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  post VARCHAR NOT NULL,
+  author_id BIGINT,
+  FOREIGN KEY (author_id) REFERENCES github_users(id)
+);
